9ca38d6e7e565f42485aae5bbc4add40e5a827cd
Fion Kou <fion@vermonster.com>
2018-08-16 15:25:48 -0400

    Load fish data into state onClick

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Order from "./Order";
 import Inventory from "./Inventory";
+import sampleFishes from "../sample-fishes";
 
 class App extends React.Component {
   state = {
@@ -18,6 +19,9 @@ class App extends React.Component {
     // Set the new fishes object to state
     this.setState({ fishes: fishes});
   };
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  }
 
   render(){
     return (
@@ -26,7 +30,10 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market"/>
         </div>
         <Order />  
-        <Inventory addFish={this.addFish} />
+        <Inventory 
+          addFish={this.addFish}
+          loadSampleFishes={this.loadSampleFishes}
+        />
       </div>
     );
   }
